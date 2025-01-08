94c98b10d3db0ed45a96d9f1d0e14ce70e3a39ea
GlobuX <globuq@gmail.com>
2025-01-08 11:37:11 +0500

    add ordersList

--- src/main/java/ru/globux/tacos/web/OrderController.java
+++ src/main/java/ru/globux/tacos/web/OrderController.java
@@ -3,8 +3,11 @@ package ru.globux.tacos.web;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -12,7 +15,6 @@ import ru.globux.tacos.TacoOrder;
 import ru.globux.tacos.User;
 import ru.globux.tacos.data.OrderRepository;
 
-import java.util.Date;
 
 @Controller
 @RequestMapping("/orders")
@@ -20,16 +22,15 @@ import java.util.Date;
 public class OrderController {
     //private Logger log = LoggerFactory.getLogger(OrderController.class);
     private final OrderRepository orderRepository;
+    private OrderProps orderProps;
 
-    public OrderController(OrderRepository orderRepository) {
+    public OrderController(OrderRepository orderRepository, OrderProps orderProps) {
         this.orderRepository = orderRepository;
+        this.orderProps = orderProps;
     }
 
     @GetMapping("/current")
     public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute TacoOrder order) {
-//        if (order.getCreatedAt() == null) {
-//            order.setCreatedAt(new Date());
-//        }
         if (order.getDeliveryName() == null) {
             order.setDeliveryName(user.getFullname());
         }
@@ -59,5 +60,12 @@ public class OrderController {
         orderRepository.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByCreatedAtDesc(user, pageable));
+       return "orderList";
     }
 }
---
+++ src/main/java/ru/globux/tacos/web/OrderProps.java
@@ -0,0 +1,25 @@
+package ru.globux.tacos.web;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "taco.orders")
+@Validated
+public class OrderProps {
+
+    @Min(value=5, message="must be between 5 and 25")
+    @Max(value=25, message="must be between 5 and 25")
+    private int pageSize = 20;
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+}
