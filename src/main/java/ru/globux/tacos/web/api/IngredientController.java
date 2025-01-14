e516ccea035622f9ca2346aadc2fcd32d1920290
GlobuX <globuq@gmail.com>
2025-01-14 23:14:23 +0500

    begin part 8 Spring in Action

---
+++ src/main/java/ru/globux/tacos/web/api/IngredientController.java
@@ -0,0 +1,36 @@
+package ru.globux.tacos.web.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.globux.tacos.Ingredient;
+import ru.globux.tacos.data.IngredientRepository;
+
+@RestController
+@RequestMapping(path = "/api/ingreditnts", produces = "application/json")
+@CrossOrigin(origins = "http://localhost:8080")
+public class IngredientController {
+    private final IngredientRepository ingRepo;
+
+    @Autowired
+    public IngredientController(IngredientRepository ingRepo) {
+        this.ingRepo = ingRepo;
+    }
+
+    @GetMapping
+    public Iterable<Ingredient> allIngredients() {
+        return ingRepo.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Ingredient saveIngredient(@RequestBody Ingredient ingredient) {
+        return ingRepo.save(ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteIngredient(@PathVariable("id") String ingredientId) {
+        ingRepo.deleteById(ingredientId);
+    }
+}
