4f4b8a35195346f92f8cf881d07b6c982126675f
GlobuX <globuq@gmail.com>
2025-01-20 18:27:11 +0500

    fix bags, worked projects

--- src/main/java/ru/globux/tacos/security/SecurityConfig.java
+++ src/main/java/ru/globux/tacos/security/SecurityConfig.java
@@ -8,21 +8,21 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity(debug = true)
 @Configuration
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
-    private final UserDetailsService userDetailsService;
+//    @Autowired
+//    private UserDetailsService userDetailsService;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+//    public SecurityConfig(UserDetailsService userDetailsService) {
+//        this.userDetailsService = userDetailsService;
+//    }
 
     @Bean
     public PasswordEncoder encoder() {
@@ -32,28 +32,32 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authManReqMatchReg -> authManReqMatchReg
-                    //.requestMatchers("/design", "/orders").hasRole("USER")
-                    .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                    .requestMatchers(HttpMethod.POST, "/api/ingredients").permitAll()
-                    .requestMatchers("/api/tacos", "/api/orders/**").permitAll()
-                    .requestMatchers(HttpMethod.PATCH, "/api/ingredients").permitAll()
-                    .requestMatchers("/**").permitAll())
-            .formLogin(httpSecFormLoginConf -> httpSecFormLoginConf
-                    .loginPage("/login"))
-            .httpBasic(name -> name
-                    .realmName("Taco Cloud"))
-            .logout(logout -> logout
-                    .logoutSuccessUrl("/"))
-            .csrf(matcher -> matcher
-                    .ignoringRequestMatchers("/actuator", "/h2-console/**", "/api/**"))
-            .headers(matcher -> matcher
-                    .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .requestMatchers("/design", "/orders").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/api/ingredients").permitAll()
+                        .requestMatchers("/api/tacos", "/api/orders/**").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "/api/ingredients").permitAll()
+                        .requestMatchers("/**").permitAll())
+                .formLogin(httpSecFormLoginConf -> httpSecFormLoginConf
+                        .loginPage("/login"))
+                .httpBasic(name -> name
+                        .realmName("Taco Cloud"))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/"))
+                .csrf(matcher -> matcher
+                        .ignoringRequestMatchers("/actuator", "/h2-console/**", "/api/**"))
+                .headers(matcher -> matcher
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
 
     @Bean
-    AuthenticationManager authenticationManager(
-            AuthenticationManagerBuilder builder, PasswordEncoder encoder) throws Exception {
-        return builder.userDetailsService(userDetailsService).passwordEncoder(encoder()).and().build();
+    AuthenticationManager authenticationManager(UserDetailsService userDetailsService,
+            HttpSecurity http, PasswordEncoder encoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        return authenticationManagerBuilder.userDetailsService(userDetailsService)
+                    .passwordEncoder(encoder).and().build();
     }
 }
+
